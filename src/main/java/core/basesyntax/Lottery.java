8fb72f88e82bfe1e18cd37fb4630b221d2375506
artem <artemnurmetow@gmail.com>
2025-07-15 15:46:50 +0200

    due to review: declared final static variables, declared class-variables, made creating of several objects in the loop;

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,10 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    public Ball getRandomBall() {
-        ColorSupplier colorSupplier = new ColorSupplier();
+    private static final int limitOfBallNumber = 101;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final Random random = new Random();
 
+    public Ball getRandomBall() {
         return new Ball(colorSupplier.getRandomColor(),
-                new Random().nextInt(101));
+                random.nextInt(limitOfBallNumber));
     }
 }
